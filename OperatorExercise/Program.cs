6574cc2311eec10f861a65f49c61055a535babe2
Kemuro <antonkemuro@gmail.com>
2021-04-02 13:27:37 -0500

    added another exercise AreaOfCircle

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,6 +6,8 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
+
+            #region        
             int a = 17;
             int b = 4;
             int quotient = a / b;
@@ -15,6 +17,29 @@ namespace OperatorExercise
             {
                 Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
             }
+            #endregion
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            #region
+            Console.WriteLine("Calculating the area of a circle:");
+            AreaOfCircle();
+           // Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+
+
+            #endregion
+        }
+
+        public static double AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double area = (Math.PI * radius * radius);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+
+            return area;
         }
     }
 }
